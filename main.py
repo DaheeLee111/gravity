9f23dabf70c3822a13ec9002a6ee03a983baedc7
DaheeLee111 <120421942+WarpCoreOfficial@users.noreply.github.com>
2022-12-25 04:36:26 +0900

    draw squre

--- main.py
+++ main.py
@@ -1,35 +1,85 @@
-import numpy
-import matplotlib.pyplot
-import seaborn
+import numpy as np
+import pygame
 
-seaborn.set_style("white")
-seaborn.set_context("talk")
-
-mass = 1
-gravity_acceleation = numpy.array([0, -9.8])
-
-start_position = numpy.array([0, 10])
-start_vector = numpy.array([0, 0])
-start_time = 0
-
-time_index = 0.01
-
-axis_times = []
-axis_positions = []
-
-while start_position[1] >= 0:
-    axis_times.append(start_time)
-    axis_positions.append(start_position)
-
-    start_position = start_position + start_vector * start_time + 0.5 * gravity_acceleation * start_time**2
-    start_time += time_index
+from OpenGL.GL import *
+from OpenGL.GLU import *
 
 
-fig, axs = matplotlib.pyplot.subplots(ncols=2, figsize=(8, 4), constrained_layout=True)
-directions = ["x", "y"]
-for i, (ax, direction) in enumerate(zip(axs, directions)):
-    ax.scatter(axis_times, numpy.array(axis_positions)[:,i], alpha=0.3, c="k")
-    ax.set_xlabel("time (s)")
-    ax.set_title(f"{direction} position (m)", pad=12)
+class Squre:
+    def __init__(self, location, x, y, z):
+        self.color = (255, 255, 255)
 
-matplotlib.pyplot.show()
+        self.center = location
+        self.x = x
+        self.y = y
+        self.z = z
+
+        self.vertices = (
+            (location[0]-x/2, location[1]-y/2, location[2]-z/2),
+            (location[0]+x/2, location[1]-y/2, location[2]-z/2),
+            (location[0]+x/2, location[1]+y/2, location[2]-z/2),
+            (location[0]-x/2, location[1]+y/2, location[2]-z/2),
+            (location[0]-x/2, location[1]-y/2, location[2]+z/2),
+            (location[0]+x/2, location[1]-y/2, location[2]+z/2),
+            (location[0]+x/2, location[1]+y/2, location[2]+z/2),
+            (location[0]-x/2, location[1]+y/2, location[2]+z/2),
+        )
+
+        self.edges = (
+            (0, 1), (1, 2), (2, 3), (3, 0),
+            (4, 5), (5, 6), (6, 7), (7, 4),
+            (0, 4), (1, 5), (2, 6), (3, 7),
+        )
+
+    def begin_and_end(func):
+        def wrapper(self):
+            glBegin(GL_LINES)
+            func(self)
+            glEnd()
+
+        return wrapper
+
+    @begin_and_end
+    def draw(self):
+        for vertice in self.edges:
+            glVertex3fv(self.vertices[vertice[0]])
+            glVertex3fv(self.vertices[vertice[1]])
+
+
+def main():
+    display = (800, 800)
+    object = Squre([0, 0, 0], 1, 1, 1)
+
+    pygame.init()
+    pygame.display.set_mode(display, pygame.DOUBLEBUF | pygame.OPENGL)
+
+    gluPerspective(35, (display[0]/display[1]), 0.5, 100.0)
+    glTranslatef(0.0, 0.0, -7)
+
+    running = True
+    while running:
+
+        # pygame 환경변수
+        pygame.time.Clock().tick(60)
+        pygame.display.flip()
+
+        # openGl 환경변수
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glEnable(GL_POINT_SMOOTH)
+        glEnable(GL_LINE_SMOOTH)
+        glEnable(GL_POLYGON_SMOOTH)
+
+        # 이벤트
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                running = False
+
+        glRotatef(1, 1, 1, 1)
+
+        # 그리기
+        object.draw()
+
+
+main()
